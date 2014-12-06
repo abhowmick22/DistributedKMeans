0e3e3afd004bf33b4deca9f3c0f170f9e6e3bb84
Abhishek Bhowmick <abhowmick22@gmail.com>
2014-12-05 21:56:47 -0500

    Finished README

--- DataGeneratorScripts/dnaClusterPoints.sh
+++ DataGeneratorScripts/dnaClusterPoints.sh
@@ -3,10 +3,8 @@
 o=input/dnaStrandsClustPoints.csv
 # Size of DNA strand
 s=20
-#Number of Points
-n=0
 #Dummy Number of Cluster
 c=10
 #Dummy Number of Points per cluster
 p=5
-python ./randomclustergen/generateDNAstrands.py -o $o -s $s -n $n -c $c -p $p
+python ./randomclustergen/generateDNAstrands.py -o $o -s $s -n 0 -c $c -p $p
